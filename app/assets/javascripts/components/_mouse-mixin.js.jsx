14b8c02069b6be3ce34c47b46cd701b92ea5c34e
Nathan Hutchison <nathan@bugherd.com>
2014-05-02 14:19:40 +1000

    derps

---
+++ app/assets/javascripts/components/_mouse-mixin.js.jsx
@@ -0,0 +1,77 @@
+/** @jsx React.DOM */
+
+var GrabieMouseMixin = {
+  getInitialState: function () {
+    return {
+      grabieMouse: {
+        mouseDown: false,
+        grabX: 0,
+        grabY: 0,
+        grabStartX: 0,
+        grabStartY: 0
+      }
+    };
+  },
+
+  setGrabieState: function(state) {
+    return this.setState({grabieMouse: state});
+  },
+
+  _handleGrabieMouseUp: function (e) {
+    window.removeEventListener('mouseup', this._handleGrabieMouseUp);
+    window.removeEventListener('mousemove', this._handleGrabieMouseMove);
+
+    this.state.grabieMouse && this.setGrabieState({mouseDown: false});
+
+    if (this.mightClick) {
+      this.grabieMightClick = false;
+    } else {
+      this.handleGrabieRelease && this.handleGrabieRelease(this.state.grabieMouse);
+    }
+  },
+
+  _handleGrabieMouseMove: function (e) {
+    if (!this._grabieMightClick && this.state.grabieMouse.mouseDown) {
+      this.setGrabieState({
+        grabX: e.pageX,
+        grabY: e.pageY
+      });
+      this.handleGrabieMove  && this.handleGrabieMove(this.state.grabieMouse);
+    }
+  },
+
+  _isLeftMouseButton: function(e) {
+    var button = e.button;
+    return !(button && (button !== 0 && button !== 1));
+  },
+
+  _handleGrabieMouseDown: function(e) {
+    if (this._isLeftMouseButton(e)) {
+      return;
+    }
+
+    window.addEventListener('mouseup', this._handleGrabieMouseUp);
+
+    this.grabieMightClick = true;
+    setTimeout(
+      function() {
+        if (this.grabieMightClick) {
+          this.mightClick = false;
+          window.addEventListener('mousemove', this._handleGrabieMouseMove);
+
+          this.setGrabieState({
+            mouseDown: true,
+            grabStartX: e.pageX,
+            grabStartY: e.pageY,
+            grabX: e.pageX,
+            grabY: e.pageY
+          });
+
+          this.handleGrabieGrab  && this.handleGrabieGrab(this.state.grabieMouse);
+        }
+      }.bind(this), 200
+    );
+
+    e.preventDefault();
+  }
+}
