22e9d97acb7e25612d813a8a66e42055a97f7d7a
Nathan Hutchison <nathan@bugherd.com>
2014-05-21 09:20:16 +1000

    derp

--- app/assets/javascripts/components/grabie.jsx
+++ app/assets/javascripts/components/grabie.jsx
@@ -47,9 +47,7 @@ var Grabbable = React.createClass({
         <div onMouseDown={this._handleGrabieMouseDown} className="grabie-grabbable">{React.Children.only(this.props.children)}</div>
       );
     } else {
-      var r = this.getRect();
       return (
-
         <Overlay
             style={{width: this.r.width, height: this.r.height, pointerEvents: 'none'}}
             x={this.state.grabieMouse.grabX - (this.state.grabieMouse.grabStartX - this.r.left)}
